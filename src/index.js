0273bf4c8845f1af341ec11844cc0a0ed1cdc0f9
Carlos-Monteiro26 <dev.carlos.monteiro@gmail.com>
2024-02-05 10:53:29 -0300

    feat: finances instances and reusable queries

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ const db = require("./db");
 const app = express();
 const port = 3000;
 
+const routesCategories = require("./routes/categories");
+const routesUsers = require("./routes/users");
+const routesFinances = require("./routes/finances");
+
 app.use(express.json());
 
-const routesCategories = require("./routes/categories");
+app.use("/categories", routesCategories);
+app.use("/users", routesUsers);
+app.use("/finances", routesFinances);
 
 app.get("/", (req, res) => {
   res.send("Olá, essa é a aplicação Wallet App");
 });
-
-app.use("/categories", routesCategories);
 
 app.listen(port, () => {
   db.connect()
